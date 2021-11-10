e7c8f7cc827c91ef52f3077ba28048a2a4e91e18
Paulo Moutinho <paulo_vicali@icloud.com>
2021-11-10 12:00:27 -0300

    Primeira aplicação com um framework JS ( Vue.Js ) - Cálculo IMC

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div class="app-imc" >
+      <h1>Cálculo IMC</h1>
+      <h2>Digite altura e peso para calcular o IMC</h2>
+      <span class="p-float-label">
+         <InputText id="input-weight" type="text" v-model="weight" v-bind:disabled="imc"/>
+         <label for="input-weight">Peso</label>
+      </span>
+      <span class="p-float-label">
+         <InputText id="input-height" type="text" v-model="height" v-bind:disabled="imc" />
+         <label for="input-height">Altura</label>
+      </span>
+      <Button label="Calcular" @click="calculate" />
+       <Button label="Limpar" @click="clear"/>
+   </div>
+
+   <div class="container" v-if="imc">
+      <h3>Seu IMC: {{imc}}</h3>
+      <h4>Classificação {{classification}} </h4>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+   export default {
+      data() {
+         return {
+            // propriedades onde os calculos serão imprimidos na tela 
+            weight: null,
+            height: null,
+            imc: null,
+            classification: ''
+         }
+      },
+      methods: {
+         calculate() {
+            this.imc = (this.weight / (this.height * this.height)).toFixed(2)
+            if (this.imc < 18.5) {
+               this.classification = 'Magreza'
+            }else if (this.imc >= 18.5 && this.imc < 25) {
+               this.classification = 'Normal'
+            }else if (this.imc >= 25 && this.imc < 30) {
+               this.classification = 'Sobrepeso'
+            }else if (this.imc >= 30 && this.imc < 40) {
+               this.classification = 'Obesidade'
+            }else if (this.imc >= 40) {
+               this.classification = 'Obesidade Grave'
+            }
+         },
+// função onde as propridades executarão o botão de limpar a tela
+         clear() {
+            this.weight = null
+            this.height = null
+            this.imc = null
+            this.result = ''
+            this.classification = ''
+         }
+      }
+   }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 1º - importamos o framework primevue através desta linha de código
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+
+// 1°.2 - importar estilos de primevue
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+
+const app = createApp(App)
+// 2º - depois de importar,usamos .use para registrar e utilizar a importação feita
+app.use(PrimeVue)
+// 3º - registrar componentes
+app.component('Button', Button)
+app.component('InputText', InputText)
+
+app.mount('#app')
